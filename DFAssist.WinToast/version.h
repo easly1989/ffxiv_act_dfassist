368aa2d8478bab794e52348e7285664af5caee9a
easly1989 <caesar.themaster@gmail.com>
2019-08-08 09:14:58 +0200

    Added a shared version to handle version updates for all the projects in the solution - Also added a versioninfo file for the WinToast project

---
+++ DFAssist.WinToast/version.h
@@ -0,0 +1,37 @@
+#pragma once
+#define STRINGIZE2(s) #s
+#define STRINGIZE(s) STRINGIZE2(s)
+
+// versions must be updated manually, based on the version of the main assembly
+#define VERSION_MAJOR               2
+#define VERSION_MINOR               0
+#define VERSION_REVISION            0
+#define VERSION_BUILD               0 
+
+#if DEBUG
+#define VER_FILE_DESCRIPTION_STR    "Flavor=Debug"
+#else
+#define VER_FILE_DESCRIPTION_STR    "Flavor=Retail"
+#endif
+#define VER_FILE_VERSION            VERSION_MAJOR, VERSION_MINOR, VERSION_REVISION, VERSION_BUILD
+#define VER_FILE_VERSION_STR        STRINGIZE(VERSION_MAJOR)        \
+                                    "." STRINGIZE(VERSION_MINOR)    \
+                                    "." STRINGIZE(VERSION_REVISION) \
+                                    "." STRINGIZE(VERSION_BUILD)    \
+ 
+#define VER_PRODUCTNAME_STR         "DFAssist.WinToast"
+#define VER_PRODUCT_VERSION         VER_FILE_VERSION
+#define VER_PRODUCT_VERSION_STR     VER_FILE_VERSION_STR
+#define VER_ORIGINAL_FILENAME_STR   VER_PRODUCTNAME_STR ".dll"
+#define VER_INTERNAL_NAME_STR       VER_ORIGINAL_FILENAME_STR
+#define VER_COPYRIGHT_STR           "Copyright © Carlo Ruggiero (easly1989) 2018/2019"
+ 
+#ifdef _DEBUG
+  #define VER_VER_DEBUG             VS_FF_DEBUG
+#else
+  #define VER_VER_DEBUG             0
+#endif
+ 
+#define VER_FILEOS                  VOS_NT_WINDOWS32
+#define VER_FILEFLAGS               VER_VER_DEBUG
+#define VER_FILETYPE                VFT_APP
