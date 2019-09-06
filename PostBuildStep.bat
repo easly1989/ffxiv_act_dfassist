27fd2a9de6c376d2ff2c18ad55c390ff390505ad
easly1989 <caesar.themaster@gmail.com>
2019-09-06 12:47:50 +0200

    Fixed PostBuilStep.bat to avoid creating unused folders

--- PostBuildStep.bat
+++ PostBuildStep.bat
@@ -5,20 +5,17 @@ set libs=libs
 set pdb=pdb
 set data=data
 set loc=localization
-set img=images
 echo %from%
 echo %libs%
 echo %pdb%
 echo %data%
 echo %loc%
-echo %img%
 echo %resources%
 echo "Creating folders if necessary..."
 if not exist %libs% mkdir %libs%
 if not exist %pdb% mkdir %pdb%
 if not exist %data% mkdir %data%
 if not exist %loc% mkdir %loc%
-if not exist %img% mkdir %img%
 echo "Moving all dll files..."
 move /Y %from%*.dll %libs%
 move /Y %from%*.config %libs%
@@ -29,7 +26,6 @@ del /F /Q %from%*.*
 echo "Copying files to resources"
 xcopy /S /Y %resources%data %data%
 xcopy /S /Y %resources%localization %loc%
-xcopy /S /Y %resources%images %img%
 echo "Moving DFAssist back to main directory..."
 move /Y %libs%\DFAssist.dll %from%DFAssist.dll
 echo "All done! :)"
