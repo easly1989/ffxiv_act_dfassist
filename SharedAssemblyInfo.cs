16ed3c2a5c0d68b9760fc10d53a72bf9097bb148
easly1989 <caesar.themaster@gmail.com>
2020-01-01 10:13:27 +0100

    Version Updated

--- SharedAssemblyInfo.cs
+++ SharedAssemblyInfo.cs
@@ -35,11 +35,11 @@ using System.Runtime.InteropServices;
 // to distinguish one build from another. AssemblyFileVersion is specified
 // in AssemblyVersionInfo.cs so that it can be easily incremented by the
 // automated build process.
-[assembly: AssemblyVersion("2.2.12")]
+[assembly: AssemblyVersion("2.2.13")]
 
 // By default, the "Product version" shown in the file properties window is
 // the same as the value specified for AssemblyFileVersionAttribute.
 // Set AssemblyInformationalVersionAttribute to be the same as
 // AssemblyVersionAttribute so that the "Product version" in the file
 // properties window matches the version displayed in the GAC shell extension.
-[assembly: AssemblyInformationalVersion("2.2.12")] // a.k.a. "Product version"
+[assembly: AssemblyInformationalVersion("2.2.13")] // a.k.a. "Product version"
